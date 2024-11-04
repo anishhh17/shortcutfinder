f66a7249e18a6fa8e8e2ec2cb7a67ec705055ba8
Anish <anishsurenda7@gmail.com>
2024-11-04 09:07:10 +0530

    side panel html working

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,27 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld-sample" is now active!');
+export function activate(context: vscode.ExtensionContext){
+  var thisProvider={
+      resolveWebviewView:function(thisWebview :any, thisWebviewContext :any, thisToken :any){
+          thisWebview.webview.options={enableScripts:true};
+          thisWebview.webview.html=getWebviewContent();
+      }
+  };
+  context.subscriptions.push(vscode.window.registerWebviewViewProvider("shortcutfinder.shortcutFinderView", thisProvider));
+}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('shortcutFinder.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+function getWebviewContent() {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+  <p>This is a para tag</p>
+  <p id = "temp"></p>
+</body>
+ 
+</html>`;
 }
