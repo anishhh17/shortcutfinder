7044c333445a9464d255048ffdac7e10f2a0ae1d
Anish <anishsurenda7@gmail.com>
2024-11-04 21:01:03 +0530

    moved html to new file and added shortcuts.json

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,19 @@
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
-export function activate(context: vscode.ExtensionContext){
-  var thisProvider={
-      resolveWebviewView:function(thisWebview :any, thisWebviewContext :any, thisToken :any){
-          thisWebview.webview.options={enableScripts:true};
-          thisWebview.webview.html=getWebviewContent();
-      }
-  };
-  context.subscriptions.push(vscode.window.registerWebviewViewProvider("shortcutfinder.shortcutFinderView", thisProvider));
+export function activate(context: vscode.ExtensionContext) {
+    const thisProvider = {
+        resolveWebviewView: function (thisWebview: any, thisWebviewContext: any, thisToken: any) {
+            thisWebview.webview.options = { enableScripts: true };
+            thisWebview.webview.html = getWebviewContent(context.extensionUri);
+        }
+    };
+    context.subscriptions.push(vscode.window.registerWebviewViewProvider("shortcutfinder.shortcutFinderView", thisProvider));
 }
 
-function getWebviewContent() {
-  return `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cat Coding</title>
-</head>
-<body>
-  <p>This is a para tag</p>
-  <p id = "temp"></p>
-</body>
- 
-</html>`;
+function getWebviewContent(extensionUri: vscode.Uri) {
+    const htmlPath = path.join(extensionUri.fsPath, 'searchBar.html');
+    const htmlContent = fs.readFileSync(htmlPath, 'utf-8');
+    return htmlContent;
 }
